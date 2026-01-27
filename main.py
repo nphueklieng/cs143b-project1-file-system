62b29ef4b8c077ca86da9296f32d98bc755d92bf
Niva Phueklieng <nphueklieng@gmail.com>
2026-01-27 14:25:32 -0800

    Design presentation shell skeleton

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
 # CS 143B - Project 1 (main.py)
 # Name: Duangchanok Phueklieng (45231967)
 
-class EmulatedDisk:
-    def __init__ (self):
-        self.D = [bytearray(512) for block in range(64)] # Byte array D[64][512]
+from emulated_disk import EmulatedDisk
+from file_system import FileSystem
 
-    def read_block (self):
+def eval (user_input):
+    command = user_input[0]
+    if command == "cr":
         pass
-
-    def write_block (self):
+    elif command == "de":
         pass
-
+    elif command == "op":
+        pass
+    elif command == "cl":
+        pass
+    elif command == "rd":
+        pass
+    elif command == "wr":
+        pass
+    elif command == "sk":
+        pass
+    elif command == "dr":
+        pass
+    elif command == "in":
+        pass
+    elif command == "rm":
+        pass
+    elif command == "wm":
+        pass
+    else:
+        raise SyntaxError
 
 def main ():
-    pass
+    ''' Run the Presentation/Test Shell '''
+    while (True):
+        try:
+            user_input = input("").strip().split()  # Parse user input
+            if not user_input:
+                continue    # Ignore if empty
+            eval(user_input)
+        except:
+            print("error")
+            break
 
 if __name__ == '__main__':
     main()
