23ba2356f6b328a4cd12122839b780fa3b6a5f9e
Niva Phueklieng <nphueklieng@gmail.com>
2026-01-27 13:33:44 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CS 143B: Project 1 - File System
+
+Instructions:
+1. In the terminal, run the command:  python3 main.py
---
+++ main.py
@@ -0,0 +1,16 @@
+# CS 143B - Project 1 (main.py)
+# Name: Duangchanok Phueklieng (45231967)
+
+# Byte Array D[64][512] to represent emulated disk
+
+def initialize ():
+    '''
+    At start-up, initialize the system as specified
+    '''
+
+
+def main ():
+    initialize()
+
+if __name__ == '__main__':
+    main()
